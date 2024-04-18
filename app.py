caac20a083f4fe46cc520de2385e9371f2a0b719
Bedanga Das <74611517+Bedanga864@users.noreply.github.com>
2024-04-18 16:18:48 +0530

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
   page_icon="📱",
 )
 
-st.sidebar.success("")
+st.sidebar.success("Select a page👆")
 
 st.title("Animal Image Detection Application")
 st.header("Image Classifier ")

f829edeb3501ee3a5e113e652ffcd84d12bb49ab
Bedanga Das <74611517+Bedanga864@users.noreply.github.com>
2024-04-18 15:53:51 +0530

    feedback.py

---
+++ pages/✍️feedback.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from streamlit_feedback import streamlit_feedback
+
+with st.form("main", clear_on_submit=True):
+    st.write('answer ...')
+   
+    feedback = streamlit_feedback(
+        feedback_type="thumbs",
+        optional_text_label="[Optional] Please provide an explanation",
+        align="flex-start"
+    )
+    st.form_submit_button('save')
+
+st.write(f"feedback log -{feedback}")

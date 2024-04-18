f4be4f3ee7328e35b6753fda701a9d6ebbd3bccc
Bedanga Das <74611517+Bedanga864@users.noreply.github.com>
2024-04-18 16:09:01 +0530

    feedback.py

--- pages/3_✍️feedback.py
+++ pages/3_✍️feedback.py
@@ -1,14 +1,22 @@
 import streamlit as st
 from streamlit_feedback import streamlit_feedback
-
+st.header("Feedback Form")
 with st.form("main", clear_on_submit=True):
-    st.write('answer ...')
+    st.write('write 👇 ...')
    
     feedback = streamlit_feedback(
         feedback_type="thumbs",
         optional_text_label="[Optional] Please provide an explanation",
         align="flex-start"
     )
-    st.form_submit_button('save')
+    st.form_submit_button('Submit Feedback')
 
+
+    # Check if the form was submitted
+    if submit_button:
+        # Return the feedback text upon form submission
+        return feedback_text
+
+    # If the form was not submitted, return None
+    return None
 st.write(f"feedback log -{feedback}")

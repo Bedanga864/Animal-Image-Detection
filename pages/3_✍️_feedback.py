e6bee9629aa2aa993f613a0d97addcdd558fc1bd
Bedanga Das <74611517+Bedanga864@users.noreply.github.com>
2024-04-19 20:36:20 +0530

    feedback.py

--- pages/3_✍️_feedback.py
+++ pages/3_✍️_feedback.py
@@ -19,11 +19,12 @@ with st.form("main", clear_on_submit=True):
         feedback_type="thumbs",
         optional_text_label="[Optional] Please write your feedback",
         align="flex-start"
-      if st.form_submit_button("Submit Feedback"):
+     
+    )
+    st.form_submit_button('save')
+   if st.form_submit_button("Submit Feedback"):
         save_feedback_to_file(feedback)
         st.success("Feedback submitted successfully!")
-    )
-    #st.form_submit_button('save')
 
 
 st.write(f"feedback log -{feedback}")

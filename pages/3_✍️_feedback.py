9b94ad6af0712cf1f6f39b3a08965bd67a990c6e
Bedanga Das <74611517+Bedanga864@users.noreply.github.com>
2024-04-19 20:00:05 +0530

    Update 3_✍️_feedback.py

--- pages/3_✍️_feedback.py
+++ pages/3_✍️_feedback.py
@@ -9,7 +9,8 @@ with st.form("main", clear_on_submit=True):
     st.write('Click any ⬇️')
    
     feedback = streamlit_feedback(
-        feedback_type="five stars",
+       # feedback_type="five stars",
+        rating = st.slider("Rate your experience (1 - 5 stars)", min_value=1, max_value=5, step=1)
         optional_text_label="[Optional] Please write your feedback",
         align="flex-start"
     )

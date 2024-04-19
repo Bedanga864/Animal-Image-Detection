bf032d29ed7b224414863811fedba19e88f55d01
Bedanga Das <74611517+Bedanga864@users.noreply.github.com>
2024-04-19 19:49:23 +0530

    feedback.py

--- pages/3_✍️_feedback.py
+++ pages/3_✍️_feedback.py
@@ -9,5 +9,5 @@ with st.form("main", clear_on_submit=True):
         optional_text_label="[Optional] Please provide an explanation",
         align="flex-start"
     )
-     st.form_submit_button('save')
+    st.form_submit_button('save')
 st.write(f"feedback log -{feedback}")

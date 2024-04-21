104aae0c1c6e5ba3e5b82a384296f3a253ea9624
Bedanga Das <74611517+Bedanga864@users.noreply.github.com>
2024-04-21 14:17:36 +0530

    feedback.py

--- pages/feedback.py
+++ pages/feedback.py
@@ -16,9 +16,6 @@ def main():
       style="min-width:100%;max-width:100%;height:539px;border:none;"
       scrolling="no"
     >
-    </iframe>
-    <script src='https://form.jotform.com/s/umd/latest/for-form-embed-handler.js'></script>
-    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-241112385506449'", "https://form.jotform.com/")</script>
     
     </iframe>
     """

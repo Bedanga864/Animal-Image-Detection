5d62bdb1149341949652a2d3cccf4cfc6923bc80
Bedanga Das <74611517+Bedanga864@users.noreply.github.com>
2024-04-21 14:10:33 +0530

    feedback.py

---
+++ pages/feedback.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+def main():
+    st.title("Embed JotForm in Streamlit")
+
+    # Paste your JotForm embed code here
+    jotform_embed_code = """
+    
+    <iframe
+      id="JotFormIFrame-241112385506449"
+      title="Feedback Form"
+      onload="window.parent.scrollTo(0,0)"
+      allowtransparency="true"
+      allow="geolocation; microphone; camera; fullscreen"
+      src="https://form.jotform.com/241112385506449"
+      frameborder="0"
+      style="min-width:100%;max-width:100%;height:539px;border:none;"
+      scrolling="no"
+    >
+    </iframe>
+    <script src='https://form.jotform.com/s/umd/latest/for-form-embed-handler.js'></script>
+    <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-241112385506449'", "https://form.jotform.com/")</script>
+    
+    </iframe>
+    """
+
+    # Display the JotForm using HTML iframe
+    st.markdown(jotform_embed_code, unsafe_allow_html=True)
+
+if __name__ == "__main__":
+    main()
